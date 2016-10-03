b29409e8bdcea3d45fb1fcdb9141702d66e430f5
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-10-03 18:38:05 +0000

    List product categoriess. Refs #115

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from product.views import ProductCategoryDetail, ProductCategoryCreate
+from product.views import ProductCategoryList, ProductCategoryDetail, ProductCategoryCreate
 
 urlpatterns = [
+    url(r'^category/', ProductCategoryList.as_view()),
     url(r'^category/detail/(?P<pk>[0-9]*)/$', 
         ProductCategoryDetail.as_view(),
         name='product-category-detail'),
--- product/views.py
+++ product/views.py
@@ -1,9 +1,13 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from django.views.generic.edit import CreateView, UpdateView
 from django.shortcuts import render
 from product.models import ProductCategory
 
 # Create your views here.
+
+class ProductCategoryList(ListView):
+    model = ProductCategory
+    context_object_name = 'product_categories'
 
 class ProductCategoryDetail(DetailView):
     model = ProductCategory
