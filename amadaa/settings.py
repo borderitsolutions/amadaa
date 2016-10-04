1d3a2c1f8d4b80c8c5140a4924e125bb531ff57e
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-10-04 17:32:14 +0000

    Some template updates.

--- amadaa/settings.py
+++ amadaa/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(os.path.dirname(os.path.realpath(__file__)), '../static'),
+]
 
 # Auth
 
