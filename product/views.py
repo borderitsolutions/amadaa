e12616a3a096fb179a322c324dc1b6d4600f251a
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-10-04 03:00:40 +0000

    Check permissions on product category edit views.

--- product/views.py
+++ product/views.py
@@ -3,6 +3,7 @@ from django.views.generic import DetailView, ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.shortcuts import render
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from product.models import ProductCategory
@@ -17,19 +18,28 @@ class ProductCategoryDetail(DetailView):
     model = ProductCategory
     context_object_name = 'product_category'
 
-class ProductCategoryCreate(SuccessMessageMixin, CreateView):
+class ProductCategoryCreate(PermissionRequiredMixin, SuccessMessageMixin, CreateView):
     model = ProductCategory
     fields = ['name']
+    permission_required = 'product.add_productcategory'
+    raise_exception = True
+    permission_denied_message = "You do not have the permission to add product categories"
     success_message = "Category %(name)s created"
 
-class ProductCategoryUpdate(SuccessMessageMixin, UpdateView):
+class ProductCategoryUpdate(PermissionRequiredMixin, SuccessMessageMixin, UpdateView):
     model = ProductCategory
     fields = ['name']
+    permission_required = 'product.edit_productcategory'
+    raise_exception = True
+    permission_denied_message = "You do not have the permission to modify product categories"
     success_message = "Category %(name)s updated"
 
-class ProductCategoryDelete(DeleteView):
+class ProductCategoryDelete(PermissionRequiredMixin, DeleteView):
     model = ProductCategory
     context_object_name = 'product_category'
+    permission_required = 'product.delete_productcategory'
+    raise_exception = True
+    permission_denied_message = "You do not have the permission to delete product categories"
     success_url = reverse_lazy('product-category-list')
     success_message = "Category removed"
     cancel_message = "Removal cancelled"
