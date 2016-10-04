424aa401806ddf536b9bc75efb1493561a5c2a5b
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-10-04 01:59:01 +0000

    Make messages show in delete view.

--- product/views.py
+++ product/views.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponseRedirect
 from django.views.generic import DetailView, ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.shortcuts import render
+from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from product.models import ProductCategory
 
@@ -15,18 +17,30 @@ class ProductCategoryDetail(DetailView):
     model = ProductCategory
     context_object_name = 'product_category'
 
-class ProductCategoryCreate(CreateView, SuccessMessageMixin):
+class ProductCategoryCreate(SuccessMessageMixin, CreateView):
     model = ProductCategory
     fields = ['name']
     success_message = "Category %(name)s created"
 
-class ProductCategoryUpdate(UpdateView, SuccessMessageMixin):
+class ProductCategoryUpdate(SuccessMessageMixin, UpdateView):
     model = ProductCategory
     fields = ['name']
     success_message = "Category %(name)s updated"
 
-class ProductCategoryDelete(DeleteView, SuccessMessageMixin):
+class ProductCategoryDelete(DeleteView):
     model = ProductCategory
     context_object_name = 'product_category'
     success_url = reverse_lazy('product-category-list')
-    success_message = "Category %(name)s removed"
+    success_message = "Category removed"
+    cancel_message = "Removal cancelled"
+
+    def post(self, request, *args, **kwargs):
+        if "cancel" in request.POST:
+            url = self.success_url
+            messages.warning(self.request, self.cancel_message)
+            return HttpResponseRedirect(url)
+        else:
+            messages.success(self.request, self.success_message)
+            return super(ProductCategoryDelete, self).delete(request, 
+                    *args, **kwargs)
+
