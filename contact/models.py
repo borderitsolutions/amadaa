335e2342120133ed1eb062f3f85938e3c690e718
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-10-07 14:03:51 +0000

    Many-to-many join between person and organization.

--- contact/models.py
+++ contact/models.py
@@ -11,6 +11,17 @@ class Person(AmadaaModel):
 
 class Organization(AmadaaModel):
     name = models.CharField(max_length=100)
+    members = models.ManyToManyField(
+            Person,
+            through='Membership',
+            through_fields=('organization', 'person'))
 
     def __str__(self):
         return "{}".format(self.name)
+
+class Membership(AmadaaModel):
+    organization = models.ForeignKey(Organization)
+    person = models.ForeignKey(Person)
+
+    def __str__(self):
+        return "{} member of {}".format(self.person, self.organization)
