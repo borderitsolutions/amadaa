0d91e6c3f652c19e3ce318f0efd278c72d96bc3f
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-10-07 01:22:05 +0000

    Initial contact model.

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(AmadaaModel):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return "{} {}".format(self.first_name, self.last_name)
