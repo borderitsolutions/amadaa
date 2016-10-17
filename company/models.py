a3b1ce155e73db9f729c1e1443de1fb2f8597d69
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2016-10-17 16:15:53 +0000

    Initial models for company and branch.

--- company/models.py
+++ company/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from contact.models import Organization
 
 # Create your models here.
+
+class Company(Organization):
+    users = models.ManyToManyField(User)
+
+    def __str__(self):
+        return "{}".format(self.name)
+
+class Branch(models.Model):
+    company = models.ForeignKey(Company)
+    branch_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "{}".format(self.name)
